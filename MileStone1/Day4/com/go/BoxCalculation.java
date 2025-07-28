33b8079b6bacac288745f8fd6214ca74daa95049
Govardhan <govardhanab109@gmail.com>
2025-07-28 22:31:09 +0530

    second day

---
+++ MileStone1/Day4/com/go/BoxCalculation.java
@@ -0,0 +1,27 @@
+package com.go;
+
+public class BoxCalculation {
+	 public static void main(String[] args) {
+	     Box box = new Box(15, 12, 6);
+
+	     double volume = box.getVolume();
+	     System.out.println("The volume of the box is: " + volume);
+	 }
+	}
+
+
+class Box {
+	double width;
+	double height;
+	double depth;
+	
+	Box(double w, double h, double d) {
+		width = w;
+		height = h;
+		depth = d;
+ }
+
+	double getVolume() {
+		return width * height * depth;
+ }
+}

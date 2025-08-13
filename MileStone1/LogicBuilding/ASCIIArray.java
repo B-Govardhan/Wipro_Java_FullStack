68432d1fada59a108d392953cc49201ad2a42373
GOVARDHAN <168160432+B-Govardhan@users.noreply.github.com>
2025-08-13 11:24:38 +0530

    uploading logic building files

---
+++ MileStone1/LogicBuilding/ASCIIArray.java
@@ -0,0 +1,8 @@
+public class ASCIIArray {
+    public static void main(String[] args) {
+        int[] arr = {65, 66, 67, 68}; 
+        for (int n : arr) {
+            System.out.print((char)n + " ");
+        }
+    }
+}
